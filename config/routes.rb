8c95b4cb22316bbca41985fe4b70002b911eb7b3
Jake Schmitz <jschmitz@gmail.com>
2020-09-07 06:45:53 -0500

    Add welcome page as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "welcome/index"
+  root "welcome#index"
+
   post "/calls", to: "calls#create"
   post "/get_digits", to: "calls#get_digits"
+  get "/workout_logs", to: "workout_logs#show"
 end
