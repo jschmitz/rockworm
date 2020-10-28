97bbc990a1efb6d06b5ea6191c4726608ddae7b2
Jake Schmitz <jschmitz@gmail.com>
2020-10-28 06:34:30 -0500

    Add phone number to welcome page display

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,6 @@
 class WelcomeController < ApplicationController
+  def index
+    # Get the contact number and strip +1 country code
+    @contact_phone_number = ENV["ROCKWORM_PHONE_NUMBER"][2..-1]
+  end
 end
