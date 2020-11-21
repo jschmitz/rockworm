70dcd5524d2be1b89b3789c401d4d83760cf3f88
Jake Schmitz <jschmitz@gmail.com>
2020-11-21 07:28:14 -0600

    Update welcome page styles

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -2,24 +2,44 @@ require "freeclimb"
 
 class MessageController < ApplicationController
   def create
-    Freeclimb.configure do |config|
-      # Configure HTTP basic authorization: fc
-      config.username = ENV["FC_ACCOUNT_ID"]
-      config.password = ENV["FC_ACCOUNT_TOKEN"]
-    end
+    puts "Show some flash!"
+    flash.now[:info] = "Thank you for the message!"
+    flash[:info] = "Thank you for the message!"
+    flash[:error] = "Thank you for the message!"
+    flash[:alert] = "Thank you for the message!"
+    @my_message = "messageing thas asdf"
 
-    api_instance = Freeclimb::DefaultApi.new
-    message_request = Freeclimb::MessageRequest.new(
-      from: ENV["ROCKWORM_PHONE_NUMBER"],
-      to: ENV["MY_PHONE_NUMBER"],
-      text: params["message"],
-    )
+    render :create
+    #if Message.send("from", "to", "text")
+    #  flash.now[:info] = "Thank you for the message!"
+    #else
+    #  flash.now[:error] = "Hmmm there was a problem sending that needs attention... can you check back later on?"
+    #end
+  end
+end
 
-    begin
-      result = api_instance.send_an_sms_message(message_request)
-      p result
-    rescue Freeclimb::ApiError => e
-      puts "Exception when calling DefaultApi->send_an_sms_message: #{e}"
-    end
+class Message
+  def self.send(from, to, text)
+    true
+    #Freeclimb.configure do |config|
+    #  # Configure HTTP basic authorization: fc
+    #  config.username = ENV["FC_ACCOUNT_ID"]
+    #  config.password = ENV["FC_ACCOUNT_TOKEN"]
+    #end
+
+    #api_instance = Freeclimb::DefaultApi.new
+    #message_request = Freeclimb::MessageRequest.new(
+    #  from: ENV["ROCKWORM_PHONE_NUMBER"],
+    #  to: ENV["MY_PHONE_NUMBER"],
+    #  text: params["message"],
+    #)
+
+    #begin
+    #  api_instance.send_an_sms_message(message_request)
+    #  true
+    #rescue Freeclimb::ApiError => e
+    #  puts "Exception when calling DefaultApi->send_an_sms_message: #{e}"
+    #  false
+    #end
   end
 end
