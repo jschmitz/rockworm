61203e5995dc379a690f8185d3bfbf70e2e5e070
Jake Schmitz <jschmitz@gmail.com>
2020-12-04 06:24:59 -0600

    Add text message capability

--- app/controllers/message_controller.rb
+++ app/controllers/message_controller.rb
@@ -2,44 +2,35 @@ require "freeclimb"
 
 class MessageController < ApplicationController
   def create
-    puts "Show some flash!"
-    flash.now[:info] = "Thank you for the message!"
-    flash[:info] = "Thank you for the message!"
-    flash[:error] = "Thank you for the message!"
-    flash[:alert] = "Thank you for the message!"
-    @my_message = "messageing thas asdf"
-
-    render :create
-    #if Message.send("from", "to", "text")
-    #  flash.now[:info] = "Thank you for the message!"
-    #else
-    #  flash.now[:error] = "Hmmm there was a problem sending that needs attention... can you check back later on?"
-    #end
+    if Message.send(params["message"])
+      render plain: "Success"
+    else
+      render plain: "Fail"
+    end
   end
 end
 
 class Message
-  def self.send(from, to, text)
-    true
-    #Freeclimb.configure do |config|
-    #  # Configure HTTP basic authorization: fc
-    #  config.username = ENV["FC_ACCOUNT_ID"]
-    #  config.password = ENV["FC_ACCOUNT_TOKEN"]
-    #end
+  def self.send(text)
+    Freeclimb.configure do |config|
+      # Configure HTTP basic authorization: fc
+      config.username = ENV["FC_ACCOUNT_ID"]
+      config.password = ENV["FC_ACCOUNT_TOKEN"]
+    end
 
-    #api_instance = Freeclimb::DefaultApi.new
-    #message_request = Freeclimb::MessageRequest.new(
-    #  from: ENV["ROCKWORM_PHONE_NUMBER"],
-    #  to: ENV["MY_PHONE_NUMBER"],
-    #  text: params["message"],
-    #)
+    api_instance = Freeclimb::DefaultApi.new
+    message_request = Freeclimb::MessageRequest.new(
+      from: ENV["ROCKWORM_PHONE_NUMBER"],
+      to: ENV["MY_PHONE_NUMBER"],
+      text: text,
+    )
 
-    #begin
-    #  api_instance.send_an_sms_message(message_request)
-    #  true
-    #rescue Freeclimb::ApiError => e
-    #  puts "Exception when calling DefaultApi->send_an_sms_message: #{e}"
-    #  false
-    #end
+    begin
+      api_instance.send_an_sms_message(message_request)
+      true
+    rescue Freeclimb::ApiError => e
+      puts "Exception when calling DefaultApi->send_an_sms_message: #{e}"
+      false
+    end
   end
 end
--- app/javascript/controllers/message_controller.js
+++ app/javascript/controllers/message_controller.js
@@ -3,7 +3,27 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = ["content", "result"]
 
-  send() {
-    this.resultTarget.innerHTML = `Under construction! ${this.contentTarget.value}`
+  async send() {
+    const csrfToken = document.head.querySelector(`meta[name="csrf-token"]`).getAttribute("content")
+    const messageContent = this.Content
+    let resultMessage = ""
+
+    let response = await fetch('/messages', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        'X-CSRF-Token': csrfToken
+      },
+      body: `{"message": "${this.contentTarget.value}"}`,
+    });
+
+    if (response.ok) {
+      resultMessage = "Thank you for the message!"
+    } else {
+      console.log("HTTP-Error: " + response.status);
+      resultMessage = "There was an error sending your message."
+    }
+
+    this.resultTarget.innerHTML = resultMessage
   }
 }
