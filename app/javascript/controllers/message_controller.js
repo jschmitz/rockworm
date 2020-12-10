909e46ea7d5ed74c0fc2a0fb8c48963e2456c46e
Jake Schmitz <jschmitz@gmail.com>
2020-12-10 07:22:43 -0600

    Add flass message and styling to message send

--- app/javascript/controllers/message_controller.js
+++ app/javascript/controllers/message_controller.js
@@ -4,9 +4,11 @@ export default class extends Controller {
   static targets = ["content", "result"]
 
   async send() {
-    const csrfToken = document.head.querySelector(`meta[name="csrf-token"]`).getAttribute("content")
+    const csrfToken = document.head.querySelector(`meta[name="csrf-token"]`).getAttribute('content')
     const messageContent = this.Content
     let resultMessage = ""
+    let className = ""
+    let contentValue = ""
 
     let response = await fetch('/messages', {
       method: 'POST',
@@ -18,12 +20,18 @@ export default class extends Controller {
     });
 
     if (response.ok) {
-      resultMessage = "Thank you for the message!"
+      resultMessage = 'Thank you for the message!'
+      className = 'alert alert-success'
+      contentValue = ""
     } else {
-      console.log("HTTP-Error: " + response.status);
-      resultMessage = "There was an error sending your message."
+      console.log('HTTP-Error: ' + response.status);
+      resultMessage = 'There was an error sending your message.'
+      className = 'alert alert-danger'
+      contentValue = ""
     }
 
     this.resultTarget.innerHTML = resultMessage
+    this.resultTarget.className = className
+    this.contentTarget.value = contentValue
   }
 }
